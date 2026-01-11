8b5cfef5b6d2163a4d4cc2a9a2b45cfce2991957
cwangg897 <dhkdrb897@gmail.com>
2026-01-11 20:12:10 +0900

    Test: 기본 판정 로직 테스트
    Signed-off-by: cwangg897 <dhkdrb897@gmail.com>

--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -18,4 +18,31 @@ class WordTest {
         // then
         Assertions.assertEquals("🟩⬜⬜🟨⬜", result.getBoard());
     }
+
+    @Test
+    void 기본_판정_로직_완전_일치() {
+        // given
+        Result result = new Result();
+        Answer answer = new Answer();
+        Input input = new Input(answer.getValue(), result);
+        Word word = new Word(input, answer);
+        // when
+        word.compareAnswer();
+        // then
+        Assertions.assertEquals("🟩🟩🟩🟩🟩", result.getBoard());
+    }
+
+    @Test
+    void 기본_판정_로직_완전_불일치() {
+        // given
+        Result result = new Result();
+        Answer answer = new Answer();
+        Input input = new Input("BVSKI", result);
+        Word word = new Word(input, answer);
+        // when
+        word.compareAnswer();
+        // then
+        Assertions.assertEquals("⬜⬜⬜⬜⬜", result.getBoard());
+    }
+
 }
